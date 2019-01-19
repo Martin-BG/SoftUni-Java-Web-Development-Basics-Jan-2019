8be85e4e69655d951fb55944e5833318c51e1156
MartinBG <MartinBG@abv.bg>
2019-01-19 05:45:49 +0200

    cleanup

--- 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/Demo.java
+++ 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/Demo.java
@@ -4,29 +4,30 @@ import improvedhttpparser.http.*;
 
 public class Demo {
 
+    private static final String HEADERS = "" +
+            "Date: 17/01/2019\r\n" +
+            "Host: localhost:8000\r\n" +
+            "Content-Type: application/xml\r\n" +
+            "Authorization: Basic UGVzaG8=\r\n";
+
     public static void main(String[] args) {
         HttpRequest postRequest = new HttpRequestImpl("" +
                 "POST /url HTTP/1.1\r\n" +
-                "Date: 17/01/2019\r\n" +
-                "Host: localhost:8000\r\n" +
-                "Content-Type: application/xml\r\n" +
-                "Authorization: Basic UGVzaG8=\r\n" +
+                HEADERS +
                 "\r\n" +
                 "name=Yum&quantity=50&price=10");
 
         HttpRequest getRequest = new HttpRequestImpl("" +
                 "GET /url HTTP/1.1\r\n" +
-                "Date: 17/01/2019\r\n" +
-                "Host: localhost:8000\r\n" +
-                "Content-Type: application/xml\r\n" +
-                "Authorization: Basic UGVzaG8=\r\n");
+                HEADERS);
+
+        System.out.println("getRequest isResource: " + getRequest.isResource());
 
         HttpRequest getResourceRequest = new HttpRequestImpl("" +
                 "GET /url/index.html HTTP/1.1\r\n" +
-                "Date: 17/01/2019\r\n" +
-                "Host: localhost:8000\r\n" +
-                "Content-Type: application/xml\r\n" +
-                "Authorization: Basic UGVzaG8=\r\n");
+                HEADERS);
+
+        System.out.println("getResourceRequest isResource: " + getResourceRequest.isResource());
 
         HttpResponse httpResponse = new HttpResponseImpl();
         httpResponse.setStatusCode(StatusCode.OK);

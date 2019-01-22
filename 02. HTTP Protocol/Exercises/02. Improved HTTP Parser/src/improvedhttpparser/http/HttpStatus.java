056e3e8f3eb3d37d65a037f9d421edda52bc868d
MartinBG <MartinBG@abv.bg>
2019-01-22 21:23:11 +0200

    add description to HTTP Statuses

--- 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/http/HttpStatus.java
+++ 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/http/HttpStatus.java
@@ -1,24 +1,36 @@
 package improvedhttpparser.http;
 
 public enum HttpStatus {
-    OK(200, "OK"),
-    BAD_REQUEST(400, "Bad Request"),
-    UNAUTHORIZED(401, "Unauthorized"),
-    NOT_FOUND(404, "Not Found");
+    OK(200, "OK", "Success"),
+
+    BAD_REQUEST(400, "Bad Request",
+            "There was an error with the requested functionality due to malformed request."),
+
+    UNAUTHORIZED(401, "Unauthorized",
+            "You are not authorized to access the requested functionality."),
+
+    NOT_FOUND(404, "Not Found",
+            "The requested functionality was not found.");
 
     private int code;
     private String name;
+    private String description;
 
-    HttpStatus(int code, String name) {
+    HttpStatus(int code, String name, String description) {
         this.code = code;
         this.name = name;
+        this.description = description;
     }
 
     public int getCode() {
-        return this.code;
+        return code;
     }
 
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }

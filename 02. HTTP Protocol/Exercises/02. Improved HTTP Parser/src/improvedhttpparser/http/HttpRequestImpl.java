188b327c65d1ceb7f08703587cffd0400629b9b7
MartinBG <MartinBG@abv.bg>
2019-01-25 01:58:47 +0200

    Preserve order of request body params

--- 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/http/HttpRequestImpl.java
+++ 02. HTTP Protocol/Exercises/02. Improved HTTP Parser/src/improvedhttpparser/http/HttpRequestImpl.java
@@ -18,7 +18,7 @@ public class HttpRequestImpl implements HttpRequest {
 
     public HttpRequestImpl(String request) {
         headers = new LinkedHashMap<>();
-        bodyParameters = new HashMap<>();
+        bodyParameters = new LinkedHashMap<>();
         init(request);
     }
 

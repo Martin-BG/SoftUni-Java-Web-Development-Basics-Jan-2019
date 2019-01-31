2b69214527bbd14b9e64c423e2e1d31c3c1ee758
MartinBG <MartinBG@abv.bg>
2019-01-31 03:27:32 +0200

    refactoring

--- 04. Introduction to Java EE/Exercise/Fluffy Duffy Munchkin Cats/src/main/java/fdmc/web/servlets/cats/CatsAllServlet.java
+++ 04. Introduction to Java EE/Exercise/Fluffy Duffy Munchkin Cats/src/main/java/fdmc/web/servlets/cats/CatsAllServlet.java
@@ -2,8 +2,8 @@ package fdmc.web.servlets.cats;
 
 
 import fdmc.domain.entities.Cat;
-import fdmc.utils.Reader;
-import fdmc.utils.TemplateEngine;
+import fdmc.utils.htmlbuilder.HtmlBuilder;
+import fdmc.utils.templatebuilder.TemplateBuilder;
 import fdmc.web.servlets.BaseServlet;
 
 import javax.inject.Inject;
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,31 +30,44 @@ public class CatsAllServlet extends BaseServlet {
     private static final String CATS_PLACEHOLDER = "cats";
 
     @Inject
-    public CatsAllServlet(Reader htmlFileReader, TemplateEngine templateEngine) {
-        super(htmlFileReader, templateEngine);
+    public CatsAllServlet(HtmlBuilder htmlBuilder) {
+        super(htmlBuilder);
+    }
+
+    private static Consumer<String> buildCatsList(Map<String, Cat> cats, Map<String, String> params) {
+        return html -> {
+            StringBuilder catsLinks = new StringBuilder();
+            cats.forEach((name, cat) ->
+                    catsLinks.append(
+                            TemplateBuilder
+                                    .from(html)
+                                    .put(PARAM_CAT_NAME, name)
+                                    .build()));
+            params.put(CATS_PLACEHOLDER, catsLinks.toString());
+        };
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        Map<String, Cat> cats = getCats();
         try {
-            Map<String, String> templatesUris = new LinkedHashMap<>();
             Map<String, String> params = new LinkedHashMap<>();
+            Map<String, String> templatesUris = new LinkedHashMap<>();
+
             templatesUris.put(HTML_SKELETON_BODY_PLACEHOLDER, URI_CATS_ALL_HTML);
+
+            Map<String, Cat> cats = getCats();
+
             if (cats.isEmpty()) {
                 templatesUris.put(CATS_PLACEHOLDER, URI_CATS_ALL_NO_CATS_HTML);
             } else {
-                Optional<String> catLinkHtml = fileReader.read(URI_CATS_ALL_CAT_LINK_HTML);
-                if (catLinkHtml.isPresent()) {
-                    StringBuilder catsLinks = new StringBuilder();
-                    cats.forEach((name, cat) -> catsLinks.append(
-                            templateEngine.applyTemplates(catLinkHtml.get(), Map.of(PARAM_CAT_NAME, name))));
-                    params.put(CATS_PLACEHOLDER, catsLinks.toString());
-                } else {
-                    notFound(resp, URI_CATS_ALL_CAT_LINK_HTML);
-                }
+                htmlBuilder
+                        .buildFrom(URI_CATS_ALL_CAT_LINK_HTML)
+                        .ifPresentOrElse(
+                                buildCatsList(cats, params),
+                                () -> notFound(resp, URI_CATS_ALL_CAT_LINK_HTML));
             }
-            loadAndBuildPage(resp, HTML_SKELETON_URI, templatesUris, params);
+
+            handleResponse(resp, templatesUris, params);
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, req.getRequestURL().toString(), e);
         }

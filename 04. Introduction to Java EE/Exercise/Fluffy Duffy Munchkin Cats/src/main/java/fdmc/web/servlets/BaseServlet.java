96d68d929c7b16ba18bc8c8340f7405af81a1d49
MartinBG <MartinBG@abv.bg>
2019-01-31 03:28:10 +0200

    refactoring

--- 04. Introduction to Java EE/Exercise/Fluffy Duffy Munchkin Cats/src/main/java/fdmc/web/servlets/BaseServlet.java
+++ 04. Introduction to Java EE/Exercise/Fluffy Duffy Munchkin Cats/src/main/java/fdmc/web/servlets/BaseServlet.java
@@ -1,15 +1,13 @@
 package fdmc.web.servlets;
 
 import fdmc.domain.entities.Cat;
-import fdmc.utils.Reader;
-import fdmc.utils.TemplateEngine;
+import fdmc.utils.htmlbuilder.HtmlBuilder;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,19 +23,17 @@ public abstract class BaseServlet extends HttpServlet {
     protected static final String PARAM_CAT_COLOR = "color";
     protected static final String PARAM_CAT_AGE = "age";
 
-    protected static final String HTML_SKELETON_URI = "/html/index.html";
     protected static final String HTML_SKELETON_BODY_PLACEHOLDER = "body";
 
-    protected final Reader fileReader;
-    protected final TemplateEngine templateEngine;
+    protected final HtmlBuilder htmlBuilder;
 
-    protected BaseServlet(Reader fileReader, TemplateEngine templateEngine) {
-        this.fileReader = fileReader;
-        this.templateEngine = templateEngine;
+    protected BaseServlet(HtmlBuilder htmlBuilder) {
+        this.htmlBuilder = htmlBuilder;
     }
 
     protected static void notFound(HttpServletResponse resp, String resource) {
-        String message = "Resource: " + resource;
+        String message = "Resource(s) not found:\r\n\t" + resource;
+        LOGGER.log(Level.SEVERE, message);
         try {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND, message);
         } catch (IOException e) {
@@ -45,28 +41,25 @@ public abstract class BaseServlet extends HttpServlet {
         }
     }
 
-    protected void loadSimplePage(HttpServletResponse resp, String uri) throws IOException {
-        try (PrintWriter writer = resp.getWriter()) {
-            Optional<String> stringOptional = fileReader.read(uri);
-            stringOptional.ifPresentOrElse(writer::write, () -> notFound(resp, uri));
+    protected static void badRequest(HttpServletResponse resp, String msg) {
+        String message = "Bad request:\r\n\t" + msg;
+        LOGGER.log(Level.SEVERE, message);
+        try {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, message);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, message, e);
         }
     }
 
-    protected void loadAndBuildPage(HttpServletResponse resp,
-                                    String skeletonUri,
-                                    Map<String, String> templatesUris,
-                                    Map<String, String> params) throws IOException {
+    protected void handleResponse(HttpServletResponse resp,
+                                  Map<String, String> templatesUris,
+                                  Map<String, String> params) throws IOException {
         try (PrintWriter writer = resp.getWriter()) {
-            Optional<String> skeleton = fileReader.read(skeletonUri);
-            Optional<Map<String, String>> templates = fileReader.read(templatesUris);
-
-            if (skeleton.isPresent() && templates.isPresent()) {
-                String html = templateEngine.applyTemplates(skeleton.get(), templates.get());
-                html = templateEngine.applyTemplates(html, params);
-                writer.write(html);
-            } else {
-                notFound(resp, skeletonUri);
-            }
+            htmlBuilder
+                    .buildFrom(templatesUris, params)
+                    .ifPresentOrElse(
+                            writer::write,
+                            () -> notFound(resp, String.join("\r\n\t", templatesUris.values())));
         }
     }
 

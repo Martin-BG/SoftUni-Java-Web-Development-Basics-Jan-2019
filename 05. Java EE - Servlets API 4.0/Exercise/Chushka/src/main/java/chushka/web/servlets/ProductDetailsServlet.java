b4c0d16b03f2ef049efa027d1ca67ff5963d4058
MartinBG <MartinBG@abv.bg>
2019-02-07 00:18:57 +0200

    refactoring, fixed random issue caused by map ordering

--- 05. Java EE - Servlets API 4.0/Exercise/Chushka/src/main/java/chushka/web/servlets/ProductDetailsServlet.java
+++ 05. Java EE - Servlets API 4.0/Exercise/Chushka/src/main/java/chushka/web/servlets/ProductDetailsServlet.java
@@ -3,7 +3,6 @@ package chushka.web.servlets;
 import chushka.domain.models.view.ProductVewModel;
 import chushka.service.ProductService;
 import chushka.utils.htmlbuilder.HtmlBuilder;
-import chushka.utils.reader.HtmlFileReader;
 
 import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
@@ -23,34 +22,25 @@ public class ProductDetailsServlet extends BaseServlet {
     private static final String PARAM_TYPE = "type";
 
     private final ProductService productService;
-    private final HtmlFileReader htmlFileReader;
 
     @Inject
-    public ProductDetailsServlet(ProductService productService,
-                                 HtmlFileReader htmlFileReader,
-                                 HtmlBuilder htmlBuilder) {
+    public ProductDetailsServlet(ProductService productService, HtmlBuilder htmlBuilder) {
         super(htmlBuilder);
         this.productService = productService;
-        this.htmlFileReader = htmlFileReader;
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        htmlFileReader
-                .read(URI_PRODUCT_DETAILS_HTML)
-                .ifPresentOrElse(html -> {
-                    String name = req.getParameter(PARAM_NAME);
-                    Optional<ProductVewModel> product = productService.findByName(name);
-                    if (product.isPresent()) {
-                        handleResponse(resp,
-                                Map.of(),
-                                Map.of(HTML_SKELETON_BODY_PLACEHOLDER, html,
-                                        PARAM_NAME, product.get().getName(),
-                                        PARAM_DESCRIPTION, product.get().getDescription(),
-                                        PARAM_TYPE, product.get().getType()));
-                    } else {
-                        badRequest(resp, "Product not found by name " + name).run();
-                    }
-                }, notFound(resp, URI_PRODUCT_DETAILS_HTML));
+        String name = req.getParameter(PARAM_NAME);
+        Optional<ProductVewModel> product = productService.findByName(name);
+        if (product.isPresent()) {
+            handleResponse(resp,
+                    Map.of(HTML_SKELETON_BODY_PLACEHOLDER, URI_PRODUCT_DETAILS_HTML),
+                    Map.of(PARAM_NAME, product.get().getName(),
+                            PARAM_DESCRIPTION, product.get().getDescription(),
+                            PARAM_TYPE, product.get().getType()));
+        } else {
+            badRequest(resp, "Product not found by name " + name).run();
+        }
     }
 }

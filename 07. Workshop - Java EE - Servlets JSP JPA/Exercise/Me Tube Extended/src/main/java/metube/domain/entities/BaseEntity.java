59869446b9660862ac3b42b82b66c7a8e3f9507f
MartinBG <MartinBG@abv.bg>
2019-02-10 19:02:45 +0200

    base entity

--- 07. Workshop - Java EE - Servlets JSP JPA/Exercise/Me Tube Extended/src/main/java/metube/domain/entities/BaseEntity.java
+++ 07. Workshop - Java EE - Servlets JSP JPA/Exercise/Me Tube Extended/src/main/java/metube/domain/entities/BaseEntity.java
@@ -19,6 +19,6 @@ class BaseEntity {
     @Id
     @GeneratedValue(generator = "uuid-string")
     @GenericGenerator(name = "uuid-string", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    @Column(name = "id", unique = true, nullable = false, updatable = false, length = 36)
     private String id;
 }

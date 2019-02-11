dc9e9fad3bd14d595333ba5e87ee435b70d70df9
MartinBG <MartinBG@abv.bg>
2019-02-11 03:18:25 +0200

    Tube repository

---
+++ 07. Workshop - Java EE - Servlets JSP JPA/Exercise/Me Tube Extended/src/main/java/metube/repositories/TubeRepository.java
@@ -0,0 +1,12 @@
+package metube.repositories;
+
+import metube.domain.entities.Tube;
+
+import java.util.Optional;
+
+public interface TubeRepository extends CrudRepository<Tube, String> {
+
+    Optional<Tube> findById(String id);
+
+    Optional<Tube> findByName(String name);
+}
---
+++ 07. Workshop - Java EE - Servlets JSP JPA/Exercise/Me Tube Extended/src/main/java/metube/repositories/TubeRepositoryImpl.java
@@ -0,0 +1,39 @@
+package metube.repositories;
+
+import metube.domain.entities.Tube;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Stateless
+public class TubeRepositoryImpl extends BaseCrudRepository<Tube, String> implements TubeRepository {
+
+    @Inject
+    public TubeRepositoryImpl(Logger logger) {
+        super(logger);
+    }
+
+    @Override
+    public Optional<Tube> findByName(String name) {
+        try {
+            return Optional.of(entityManager
+                    .createNamedQuery("Tube.findByTitle", Tube.class)
+                    .setParameter("title", name)
+                    .getSingleResult());
+        } catch (IllegalStateException | PersistenceException e) {
+            logger.log(Level.SEVERE, "No Tube found by name: " + name, e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Tube> findById(String id) {
+        return read(id);
+    }
+}
+
+

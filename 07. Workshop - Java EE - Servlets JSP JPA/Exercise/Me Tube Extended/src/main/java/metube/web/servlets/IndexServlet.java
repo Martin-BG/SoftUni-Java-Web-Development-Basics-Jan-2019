b4646bb42c9911fea50385408b09d2b57922e302
MartinBG <MartinBG@abv.bg>
2019-02-11 05:02:00 +0200

    IndexServlet

---
+++ 07. Workshop - Java EE - Servlets JSP JPA/Exercise/Me Tube Extended/src/main/java/metube/web/servlets/IndexServlet.java
@@ -0,0 +1,21 @@
+package metube.web.servlets;
+
+import metube.web.WebConstants;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(WebConstants.URL_INDEX_SERVLET)
+public class IndexServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(WebConstants.JSP_INDEX).forward(req, resp);
+    }
+}

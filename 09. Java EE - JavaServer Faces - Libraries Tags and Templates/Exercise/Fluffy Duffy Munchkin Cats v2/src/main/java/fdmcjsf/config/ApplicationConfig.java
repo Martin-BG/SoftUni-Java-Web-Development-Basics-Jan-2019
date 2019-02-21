eb9fadfda8898c1f72ea58be06a7781ac251504c
MartinBG <MartinBG@abv.bg>
2019-02-21 02:42:24 +0200

    set timezone to UTC (match JDBC config)

--- 09. Java EE - JavaServer Faces - Libraries Tags and Templates/Exercise/Fluffy Duffy Munchkin Cats v2/src/main/java/fdmcjsf/config/ApplicationConfig.java
+++ 09. Java EE - JavaServer Faces - Libraries Tags and Templates/Exercise/Fluffy Duffy Munchkin Cats v2/src/main/java/fdmcjsf/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package fdmcjsf.config;
 
 import org.modelmapper.ModelMapper;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.annotation.FacesConfig;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
 @FacesConfig(version = FacesConfig.Version.JSF_2_3)
@@ -23,5 +25,10 @@ public class ApplicationConfig {
     @Dependent
     public Logger getLogger(InjectionPoint p) {
         return Logger.getLogger(p.getMember().getDeclaringClass().getName());
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }

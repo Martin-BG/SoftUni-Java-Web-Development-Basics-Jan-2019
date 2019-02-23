4c8e55caa324e0d6dce159f8a64f2add94ee9624
MartinBG <MartinBG@abv.bg>
2019-02-23 17:16:27 +0200

    login bb

---
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/web/beans/user/LoginBacking.java
@@ -0,0 +1,41 @@
+package sbojbg.web.beans.user;
+
+import sbojbg.domain.models.binding.user.UserLoginBindingModel;
+import sbojbg.domain.models.view.user.UserLoggedViewModel;
+import sbojbg.services.UserService;
+import sbojbg.web.beans.BaseBackingBean;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Model
+public class LoginBacking extends BaseBackingBean {
+
+    @Inject
+    private UserService service;
+
+    @Inject
+    private HttpServletRequest request;
+
+    private UserLoginBindingModel model = new UserLoginBindingModel();
+
+    public void login() {
+        service.login(model)
+                .ifPresent(user -> {
+                    addUserToSession(user);
+                    redirect("home");
+                });
+    }
+
+    public UserLoginBindingModel getModel() {
+        return model;
+    }
+
+    private void addUserToSession(UserLoggedViewModel user) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userId", user.getId());
+        session.setAttribute("username", user.getUsername());
+    }
+}

ccfdd955fd729917887f7e7f74ab7cdf9bc51da9
MartinBG <MartinBG@abv.bg>
2019-02-23 17:15:53 +0200

    base backing bean

---
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/web/beans/BaseBackingBean.java
@@ -0,0 +1,23 @@
+package sbojbg.web.beans;
+
+import javax.faces.context.ExternalContext;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class BaseBackingBean {
+
+    @Inject
+    protected ExternalContext externalContext;
+    @Inject
+    private Logger logger;
+
+    protected void redirect(String url) {
+        try {
+            externalContext.redirect(url);
+        } catch (IOException | IllegalArgumentException | IllegalStateException e) {
+            logger.log(Level.SEVERE, "Failed redirect to: " + url, e);
+        }
+    }
+}

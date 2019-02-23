c21ab6fb4c8240a50e9c03ea13c39060a7364e35
MartinBG <MartinBG@abv.bg>
2019-02-23 22:27:37 +0200

    Job Application - all

--- 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/services/JobApplicationService.java
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/services/JobApplicationService.java
@@ -2,10 +2,15 @@ package sbojbg.services;
 
 import sbojbg.domain.entities.JobApplication;
 import sbojbg.domain.models.binding.jobapp.JobApplicationAddBindingModel;
+import sbojbg.domain.models.view.Viewable;
+
+import java.util.List;
 
 public interface JobApplicationService extends Service<JobApplication, String> {
 
     boolean add(JobApplicationAddBindingModel model);
 
     boolean delete(String id);
+
+    <M extends Viewable<JobApplication>> List<M> all(Class<M> clazz);
 }
--- 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/services/JobApplicationServiceImpl.java
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/services/JobApplicationServiceImpl.java
@@ -3,10 +3,12 @@ package sbojbg.services;
 import org.modelmapper.ModelMapper;
 import sbojbg.domain.entities.JobApplication;
 import sbojbg.domain.models.binding.jobapp.JobApplicationAddBindingModel;
+import sbojbg.domain.models.view.Viewable;
 import sbojbg.repositories.JobApplicationRepository;
 
 import javax.inject.Inject;
 import javax.validation.Validator;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class JobApplicationServiceImpl extends BaseService<JobApplication, String, JobApplicationRepository>
@@ -34,5 +36,10 @@ public class JobApplicationServiceImpl extends BaseService<JobApplication, Strin
     @Override
     public boolean delete(String id) {
         return repository.delete(id);
+    }
+
+    @Override
+    public <M extends Viewable<JobApplication>> List<M> all(Class<M> clazz) {
+        return findAll(clazz);
     }
 }

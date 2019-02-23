f9de5ba84b82c4c188b71ef83f99cee3c707004f
MartinBG <MartinBG@abv.bg>
2019-02-23 12:40:15 +0200

    implement cache of static resources

---
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/web/filters/CacheFilter.java
@@ -0,0 +1,21 @@
+package sbojbg.web.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/faces/javax.faces.resource/*")
+public class CacheFilter implements Filter {
+
+    private static final long MAX_AGE = 60 * 60 * 24 * 30L; // 30 days in seconds
+    private static final String MAX_AGE_HEADER = "max-age=" + MAX_AGE + ", public";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Cache-Control", MAX_AGE_HEADER);
+        chain.doFilter(request, response);
+    }
+}

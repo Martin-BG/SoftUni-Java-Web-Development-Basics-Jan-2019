438ce6a07b6c13b3821e1fe65b6d41c2bf705ba1
MartinBG <MartinBG@abv.bg>
2019-02-23 23:05:23 +0200

    home view

---
+++ 11. Exam Preparations/Sboj.bg/src/main/java/sbojbg/web/beans/user/HomeBacking.java
@@ -0,0 +1,29 @@
+package sbojbg.web.beans.user;
+
+import sbojbg.domain.models.view.jobapp.JobApplicationViewModel;
+import sbojbg.services.JobApplicationService;
+import sbojbg.web.beans.BaseBackingBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+
+@Model
+public class HomeBacking extends BaseBackingBean {
+
+    @Inject
+    private JobApplicationService service;
+
+    private List<JobApplicationViewModel> jobs;
+
+    @PostConstruct
+    private void init() {
+        jobs = service.all(JobApplicationViewModel.class);
+    }
+
+    public List<JobApplicationViewModel> getJobs() {
+        return Collections.unmodifiableList(jobs);
+    }
+}

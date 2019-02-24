114b830405cdae678ea8893efa3da05ce6ce58ec
MartinBG <MartinBG@abv.bg>
2019-02-24 11:12:42 +0200

    document schedule backing bean

---
+++ 12. Exam/Final Exam/src/main/java/org/softuni/exam/web/beans/document/ScheduleBacking.java
@@ -0,0 +1,27 @@
+package org.softuni.exam.web.beans.document;
+
+import org.softuni.exam.domain.models.binding.document.DocumentScheduleBindingModel;
+import org.softuni.exam.services.DocumentService;
+import org.softuni.exam.web.beans.BaseBackingBean;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+@Model
+public class ScheduleBacking extends BaseBackingBean {
+
+    private final DocumentScheduleBindingModel model = new DocumentScheduleBindingModel();
+    @Inject
+    private DocumentService service;
+
+    public void schedule() {
+        service.schedule(model)
+                .ifPresentOrElse(
+                        id -> redirect("/details?=" + id),
+                        addMessageRunnable("Document scheduling failed. Please retry or contact support."));
+    }
+
+    public DocumentScheduleBindingModel getModel() {
+        return model;
+    }
+}

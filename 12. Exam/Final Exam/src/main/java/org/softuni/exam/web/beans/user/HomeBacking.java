cf5ff2787c3cc84c0a03de2f129c78b9186b394e
MartinBG <MartinBG@abv.bg>
2019-02-24 12:41:09 +0200

    home backing bean

---
+++ 12. Exam/Final Exam/src/main/java/org/softuni/exam/web/beans/user/HomeBacking.java
@@ -0,0 +1,46 @@
+package org.softuni.exam.web.beans.user;
+
+import org.softuni.exam.domain.models.view.document.DocumentShortViewModel;
+import org.softuni.exam.services.DocumentService;
+import org.softuni.exam.web.beans.BaseBackingBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+
+@Model
+public class HomeBacking extends BaseBackingBean {
+
+    @Inject
+    DocumentService service;
+
+    List<DocumentShortViewModel> documents = List.of();
+
+    private static String getShortTitle(String title) {
+        int maxTitleLength = 15;
+        if (title.length() > maxTitleLength) {
+            return title.substring(0, maxTitleLength - 3) + "...";
+        }
+        return title;
+    }
+
+    @PostConstruct
+    private void init() {
+        documents = service.findAll(DocumentShortViewModel.class);
+        documents.forEach(d -> d.setTitle(getShortTitle(d.getTitle())));
+    }
+
+    public List<DocumentShortViewModel> getDocuments() {
+        return Collections.unmodifiableList(documents);
+    }
+
+    public void details(String id) {
+        redirect("/details?id=" + id);
+    }
+
+    public void print(String id) {
+        redirect("/print?id=" + id);
+    }
+}

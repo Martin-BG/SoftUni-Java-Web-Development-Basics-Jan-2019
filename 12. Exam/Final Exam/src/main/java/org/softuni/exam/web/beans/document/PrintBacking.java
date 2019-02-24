1ee8327dd2336e33485e453fb222c1102264f44a
MartinBG <MartinBG@abv.bg>
2019-02-24 13:12:17 +0200

    print backing

---
+++ 12. Exam/Final Exam/src/main/java/org/softuni/exam/web/beans/document/PrintBacking.java
@@ -0,0 +1,46 @@
+package org.softuni.exam.web.beans.document;
+
+import org.softuni.exam.domain.models.view.document.DocumentDetailsViewModel;
+import org.softuni.exam.services.DocumentService;
+import org.softuni.exam.web.beans.BaseBackingBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.faces.annotation.RequestParameterMap;
+import javax.inject.Inject;
+import javax.persistence.NoResultException;
+import java.util.Map;
+
+@Model
+public class PrintBacking extends BaseBackingBean {
+
+    private DocumentDetailsViewModel model = new DocumentDetailsViewModel();
+
+    @Inject
+    private DocumentService service;
+
+    @Inject
+    @RequestParameterMap
+    private Map<String, String> requestMap;
+
+    @PostConstruct
+    public void init() {
+        String id = requestMap.get("id");
+        model = service
+                .findById(id, DocumentDetailsViewModel.class)
+                .orElseThrow(() -> new NoResultException("Document not found"));
+    }
+
+    public DocumentDetailsViewModel getModel() {
+        return model;
+    }
+
+    public void print() {
+        String id = requestMap.get("id");
+        if (service.print(id)) {
+            redirect("/home");
+        } else {
+            addMessage("Printing failed. Please retry or contact support.");
+        }
+    }
+}

58289b79b6ceac1fc04a6b19e61f3ffd1d218ee3
MartinBG <MartinBG@abv.bg>
2019-02-24 11:10:21 +0200

    document schedule binding model

---
+++ 12. Exam/Final Exam/src/main/java/org/softuni/exam/domain/models/binding/document/DocumentScheduleBindingModel.java
@@ -0,0 +1,29 @@
+package org.softuni.exam.domain.models.binding.document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.softuni.exam.domain.entities.Document;
+import org.softuni.exam.domain.models.binding.Bindable;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DocumentScheduleBindingModel implements Bindable<Document> {
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @NotNull
+    @Size(min = 1)
+    @Lob
+    @Column(nullable = false)
+    private String content;
+}

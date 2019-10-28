050d31e9e546171b42cdfb7a775c98f259d01dba
MartinBG <MartinBG@abv.bg>
2019-10-28 12:32:53 +0200

    User entity

---
+++ 10. Workshop - Java EE - JSF JPA/Exercise/PANDA/src/main/java/panda/domain/entity/User.java
@@ -0,0 +1,51 @@
+package panda.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import panda.domain.entity.role.Role;
+import panda.domain.entity.role.RoleConverter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_users_username", columnNames = {"username"}),
+                @UniqueConstraint(name = "uk_users_email", columnNames = {"email"}),
+        }
+)
+@NamedQuery(name = "User.findByUsername",
+        query = "SELECT u FROM User u WHERE u.username = :username")
+public class User extends BaseUuidEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(min = 1, max = 32)
+    @Column(nullable = false, length = 32)
+    private String username;
+
+    @NotNull
+    @Size(min = 75, max = 75)
+    @Column(nullable = false, length = 75)
+    private String password;
+
+    @NotNull
+    @Email
+    @Size(min = 1, max = 64)
+    @Column(nullable = false, length = 64)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false, length = 12)
+    @Convert(converter = RoleConverter.class)
+    private Role role;
+}

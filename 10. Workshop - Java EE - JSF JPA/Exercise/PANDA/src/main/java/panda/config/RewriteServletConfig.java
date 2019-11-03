25561cb5ebda5f1b8a3e8aacab92243e12a33e39
MartinBG <MartinBG@abv.bg>
2019-11-03 17:17:52 +0200

    update RewriteServlet: "/" -> to "guest/index" for guests or "user/home" for authenticated users

--- 10. Workshop - Java EE - JSF JPA/Exercise/PANDA/src/main/java/panda/config/RewriteServletConfig.java
+++ 10. Workshop - Java EE - JSF JPA/Exercise/PANDA/src/main/java/panda/config/RewriteServletConfig.java
@@ -2,17 +2,16 @@ package panda.config;
 
 import org.ocpsoft.logging.Logger;
 import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
-import org.ocpsoft.rewrite.config.Configuration;
-import org.ocpsoft.rewrite.config.ConfigurationBuilder;
-import org.ocpsoft.rewrite.config.Direction;
-import org.ocpsoft.rewrite.config.Log;
+import org.ocpsoft.rewrite.config.*;
 import org.ocpsoft.rewrite.context.EvaluationContext;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
 import org.ocpsoft.rewrite.servlet.config.HttpOperation;
 import org.ocpsoft.rewrite.servlet.config.Path;
 import org.ocpsoft.rewrite.servlet.config.Redirect;
 import org.ocpsoft.rewrite.servlet.config.rule.Join;
+import org.ocpsoft.rewrite.servlet.http.event.HttpInboundServletRewrite;
 import org.ocpsoft.rewrite.servlet.http.event.HttpServletRewrite;
+import org.ocpsoft.rewrite.servlet.impl.HttpInboundRewriteImpl;
 
 import javax.servlet.ServletContext;
 
@@ -30,13 +29,24 @@ public class RewriteServletConfig extends HttpConfigurationProvider {
 
     @Override
     public Configuration getConfiguration(final ServletContext context) {
+
+
+        Condition authenticated = (rewrite, evaluationContext) -> {
+            HttpInboundServletRewrite httpServletRewrite = (HttpInboundRewriteImpl) rewrite;
+            Object username = httpServletRewrite.getRequest().getSession().getAttribute("username");
+            return username != null;
+        };
+
+        Condition guest = (rewrite, evaluationContext) -> !authenticated.evaluate(rewrite, evaluationContext);
+
+        // @formatter:off
         return ConfigurationBuilder.begin()
-                .addRule()
+            .addRule()
                 .when(Direction.isInbound().and(Path.matches("/{path}")))
                 .perform(Log.message(Logger.Level.INFO, "Client requested path: {path}"))
                 .where("path").matches(".*")
 
-                .addRule()
+            .addRule()
                 .when(Direction.isInbound().and(Path.matches("/logout")))
                 .perform(new HttpOperation() {
                     @Override
@@ -45,13 +55,20 @@ public class RewriteServletConfig extends HttpConfigurationProvider {
                     }
                 }.and(Redirect.temporary(context.getContextPath() + "/")))
 
-                .addRule()
+            .addRule()
                 .when(Direction.isInbound().and(Path.matches("/index")))
                 .perform(Redirect.temporary(context.getContextPath() + "/"))
 
-                .addRule(Join.path("/").to(FACES_VIEW_GUEST + "/index.xhtml"))
-                .addRule(Join.path("/home").to(FACES_VIEW_USER + "/home.xhtml"))
-                .addRule(Join.path("/login").to(FACES_VIEW_USER + "/login.xhtml"))
-                .addRule(Join.path("/register").to(FACES_VIEW_USER + "/register.xhtml"));
+            .addRule(Join.path("/").to(FACES_VIEW_GUEST + "/index.xhtml"))
+                .when(Direction.isInbound().and(Path.matches("/")).and(guest))
+
+            .addRule(Join.path("/").to(FACES_VIEW_USER + "/home.xhtml"))
+                .when(Direction.isInbound().and(Path.matches("/")).and(authenticated))
+
+            .addRule(Join.path("/login").to(FACES_VIEW_GUEST + "/login.xhtml"))
+            .addRule(Join.path("/register").to(FACES_VIEW_GUEST + "/register.xhtml"))
+
+            ;
+        // @formatter:on
     }
 }
